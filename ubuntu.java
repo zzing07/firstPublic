a30ac48b63b209cc30423e9f1ec0e8c70963ff2a
Zing <obalaban1@hotmail.com>
2023-03-29 03:21:12 -0700

    added first function

--- ubuntu.java
+++ ubuntu.java
@@ -3,4 +3,8 @@ public class ubuntu{
     public static void main(String[] args) {
         System.out.println("here we go in ubuntu!");
     }
+
+    public void firstFunc(){
+        System.out.println("adding first function");
+    }
 }
