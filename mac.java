d29615378770eb900ad76bf3ce85bd7719206495
SingT <61646543+Sing07@users.noreply.github.com>
2023-03-29 03:16:59 -0700

    added extra line in mac file

--- mac.java
+++ mac.java
@@ -2,5 +2,7 @@ public class mac {
 
     public static void main(String[] args) {
         System.out.println("here we go in mac.java");
+        System.out.println("added another lines");
     }
+
 }
