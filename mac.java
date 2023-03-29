d4b9ba9cd7889d6626e0c2d6fbcfc4d9e451100a
SingT <61646543+Sing07@users.noreply.github.com>
2023-03-29 03:38:49 -0700

    line 13 on mac.java

--- mac.java
+++ mac.java
@@ -10,6 +10,7 @@ public class mac {
         System.out.println("im starting to get it");
         System.out.println("added from mac 2");
         System.out.println("last one on mac2 before i push to repo");
+        System.out.println("deleted branched on github and push again");
     }
 
 }
