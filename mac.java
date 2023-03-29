ca8e1f29d203bdadbc5c0c7aa887a6380f36113e
SingT <61646543+Sing07@users.noreply.github.com>
2023-03-29 11:53:34 -0700

    29 morning mac line 6

--- mac.java
+++ mac.java
@@ -3,6 +3,7 @@ public class mac {
     public static void main(String[] args) {
         System.out.println("here we go in mac.java");
         System.out.println("added another lines");
+        System.out.println("29 morning");
     }
 
     public void macFunc(){
